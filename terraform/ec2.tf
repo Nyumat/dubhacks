e8c6b54a9585cc60ed6018503a2012f9f264dc67
Thomas <nyumat18@gmail.com>
2024-10-13 10:30:00 -0700

    Use Variables for TF

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "dubjam_instance" {
   ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+  instance_type = vars.instance_type
   subnet_id     = aws_subnet.dubjam_subnet.id
   security_groups = [aws_security_group.dubjam_sg.name]
   
