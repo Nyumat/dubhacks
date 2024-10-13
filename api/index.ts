93bc62d799ea31e391fb4c81753e9f21978e34cf
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2024-10-13 02:01:29 -0700

    mongoose setup

---
+++ api/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const port = 8080;
+
+// database
+mongoose
+  .connect(process.env.DB_CONNECT ?? "")
+  .then(() => console.log("Successfully connected to database."))
+  .catch((err) => console.log(err));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  // send a simple json response
+  res.json({ message: "Hello World!" });
+});
+
+// req.isAuthenticated is provided from the auth router
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
